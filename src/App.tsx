f6072786c72617ca0b9f076ca3df56336332b3a8
Vlad <v@gmail.com>
2023-01-10 12:55:20 +0300

    project structure created

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
